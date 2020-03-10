7da9ea14fb8a50aa781d1c3435721b3eae80b09e
Prakash Kachhadiya <pbksoft2019@gmail.com>
2020-03-10 11:39:42 +0000

    login page deleted

--- src/main/java/WebTest/HomePage.java
+++ src/main/java/WebTest/HomePage.java
@@ -4,7 +4,37 @@ import org.openqa.selenium.By;
 
 public class HomePage extends Utils
 {
-    //
+    // locator for username field
+    private By _userName = By.id( "txtUsername" );
+    // locator for password field
+    private By _password = By.id( "txtPassword" );
+    // locator for login button
+    private By _LoginButton = By.id( "btnLogin" );
+    // locator for actual error message field
+    private By _actual = By.id( "spanMessage" );
+
+    // method for entering login details
+    public void enterLoginDetails(String Username, String Password)
+    {
+        // input for username from feature file
+        enterText( _userName, Username );
+        // input for password from feature file
+        enterText( _password, Password );
+    }
+    // method for click on login button
+    public void clickOnLogin()
+    {
+        clickOnElement( _LoginButton );
+    }
+    // assert method for error message
+    public void userShouldSeeErrorMessage(String ErrorMessage)
+    {
+        // string expected message from feature field
+        String expected = ErrorMessage;
+        // compare actual and expected
+        assertTextMessage( "Your Log in is unsuccessful  ", expected, _actual );
+    }
+    // method to verify that user is on home page
     public void verifyUserIsOnHomePage()
     {
         assertURL( "https://opensource-demo.orangehrmlive.com/" );
--- src/test/java/WebTest/MyStepdefs.java
+++ src/test/java/WebTest/MyStepdefs.java
@@ -7,8 +7,6 @@ import io.cucumber.java.en.When;
 
 public class MyStepdefs
 {
-    // object created for LogIn page to call methods from particular page
-    LogIn logIn = new LogIn();
     // object created for HomePage to call methods from homepage
     HomePage homePage = new HomePage();
 
@@ -23,21 +21,21 @@ public class MyStepdefs
     public void userEntersUsernameAsAndPasswordAs(String Username, String Password)
     {
         // method to enter log in details
-        logIn.enterLoginDetails( Username, Password );
+        homePage.enterLoginDetails( Username, Password );
     }
     // next step user clicks on log in button
     @And("user clicks on Login")
     // method to click on log in button
     public void userClicksOnLogin()
     {
-        logIn.clickOnLogin();
+        homePage.clickOnLogin();
     }
     // method to see error message
     @Then("user should see {string}")
     public void userShouldSeeErrorMessage(String ErrorMessage)
     {
         // error message method
-        logIn.userShouldSeeErrorMessage(ErrorMessage);
+        homePage.userShouldSeeErrorMessage(ErrorMessage);
     }
 }
 
